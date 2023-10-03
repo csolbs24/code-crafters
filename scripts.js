0f0862e8aaa4bae1b671c3235c30e07c4fbf14ba
Joseph Turgoose <21jturgoose@gmail.com>
2023-10-03 13:45:50 -0600

    Co-authored-by: Josue Avila <jojoavila@users.noreply.github.com>

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Authentication
+// Host Function - make a new ID - make a firebase database with that ID - Display ID in HTML
+// Join Function - Read ID - Questions firebase for that ID - If successful, initiate game for both users
+// Initiate Game - Update players in database - signal to update html
+
+// Update card HTML
+// Update played cards HTML
+// Update HMTL for bluff cards
+// HTML for overflow counts
+
+// Timer
