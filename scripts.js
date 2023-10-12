ce11dd0b7e281f29ed6b614982d1fa68077e9028
matthewscoresby <63676677+matthewscoresby@users.noreply.github.com>
2023-10-12 13:20:17 -0600

    Added the Modules

--- scripts.js
+++ scripts.js
@@ -8,62 +8,18 @@
 // Update HMTL for bluff cards
 // HTML for overflow counts
 
-import { default as countdown } from "./timer";
-
-// Attach the function to the window.onload event
-
-// Convert these to functions
-window.onload = function(event)
-{
-  const letter = "N"
-  const PlayerHand = document.getElementById("CurrentPlayerHand");
-  const cardTemplate = `<div class="card"><p>`+letter+`</p></div>`;
-  PlayerHand.insertAdjacentHTML("beforeend", cardTemplate);
-};
-
-window.onload = function(event)
-{
-  const letter1 = "X"
-  const cardTemplate1 = `<div class="card"><p>`+letter1+`</p></div>`;
-
-  const letter2 = "X"
-  const cardTemplate2 = `<div class="card"><p>`+letter2+`</p></div>`;
-
-  const playerBet = document.getElementById("PlayerBet");
-  playerBet.insertAdjacentHTML("beforeend", cardTemplate1);
-  playerBet.insertAdjacentHTML("beforeend", cardTemplate2);
-}
-
-window.onload = function(event)
-{
-  const letter = "P";
-  const playerPlayed = document.getElementById("PlayField");
-  const cardTemplate = `<div class="card"><p>`+letter+`</p></div>`;
-  playerPlayed.innerHTML = cardTemplate;
-}
-
-
-
-
-// Timer
-
-// Function to generate a random 4-digit number
-function generateRandomNumber() {
-    const min = 1000; // Minimum 4-digit number
-    const max = 9999; // Maximum 4-digit number
-    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    return randomNum;
-}
-
-// Function to handle button click
-function handleButtonClick() {
-    const randomNumber = generateRandomNumber();
-    document.getElementById('gameID').textContent = `GAMEID: ${randomNumber}`;
-}
+import { default as generateRandomNumber } from "./modules/generateRandomNumber.mjs";
 
 // Attach click event listener to the button
 document.getElementById('HostButton').addEventListener('click', handleButtonClick);
 
+// Function to handle button click
+function handleButtonClick() {
+  const randomNumber = generateRandomNumber();
+  document.getElementById('gameID').textContent = `GAMEID: ${randomNumber}`;
+}
+
+// Timer
 var timeLeft = 30;
 var elem = document.getElementById('timer');
 
